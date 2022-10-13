4c1e9d5e857718edf6c54931aed5da0fa5f26722
Alexcmad <113714949+Alexcmad@users.noreply.github.com>
2022-10-12 21:46:30 -0500

    Yeah started makin modudules
    Added find_letter

--- alexpy.py
+++ alexpy.py
@@ -3,6 +3,13 @@ def echo():
 
 
 def del_before(strng, char):
+    """
+        Returns a string with everything before a specified character deleted
+
+        Usage: del_after(string, char)
+
+        E.g: del_after("C:/users/username/Desktop/file.txt","file.txt") ==> "file.txt"
+    """
     string = ''
     for i in strng:
         string += i
@@ -11,10 +18,29 @@ def del_before(strng, char):
     return string
 
 
-def del_after(strng, char):
-    string = ''
-    for i in strng:
+def del_after(string, char):
+    """
+    Returns a string with everything after a specified character deleted
+
+    Usage: del_after(string, char)
+
+    E.g: del_after("username@email.com","@") ==> "username"
+    """
+    strng = ''
+    for i in string:
         if i == char:
-            return string
-        string += i
+            return strng
+        strng += i
+
+
+def find_letter(x, y):
+    """
+    Returns a list of all the indexes where the character was found
+
+    Usage: find_letter(string, character)
+
+    E.g: find_letter("mississippi", "i") ==> [1, 4, 7, 10]
+    """
+    return [idx for idx, value in enumerate(x) if value == y]
+
 
